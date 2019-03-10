bde9b494b126f2af68548cbe4a011162af90ff6d
JJ Kasper <jj@jjsweb.site>
2019-03-10 15:56:23 -0500

    Test removing worker-farm

---
+++ dist/build/webpack/plugins/terser-webpack-plugin/src/TaskRunner.js
@@ -0,0 +1,118 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const os_1 = __importDefault(require("os"));
+const async_sema_1 = __importDefault(require("async-sema"));
+const cacache_1 = __importDefault(require("cacache"));
+const minify_1 = __importDefault(require("./minify"));
+const child_process_1 = require("child_process");
+const find_cache_dir_1 = __importDefault(require("find-cache-dir"));
+const serialize_javascript_1 = __importDefault(require("serialize-javascript"));
+const workerPath = require.resolve('./worker');
+class TaskRunner {
+    constructor() {
+        this.cacheDir = find_cache_dir_1.default({ name: 'next-minifier' });
+        // In some cases cpus() returns undefined
+        // https://github.com/nodejs/node/issues/19022
+        const cpus = os_1.default.cpus() || { length: 1 };
+        this.concurrency = cpus.length - 1 || 1;
+        this.workers = [];
+        this.sema = new async_sema_1.default(this.concurrency);
+    }
+    async createWorker() {
+        const newWorker = child_process_1.fork(workerPath, [], { stdio: 'inherit' });
+        await new Promise(resolve => {
+            const waitReady = msg => {
+                if (msg.type === 'ready') {
+                    newWorker.removeListener('message', waitReady);
+                    resolve();
+                }
+            };
+            newWorker.on('message', waitReady);
+        });
+        this.workers.push(newWorker);
+    }
+    async runTask(options) {
+        if (this.concurrency > 1) {
+            // Create worker since one isn't available
+            if (!this.workers.length)
+                await this.createWorker();
+            const worker = this.workers.shift();
+            const result = await new Promise(resolve => {
+                const cleanup = () => {
+                    worker.removeListener('message', waitResult);
+                    worker.removeListener('close', handleClose);
+                };
+                const handleClose = (code, signal) => {
+                    cleanup();
+                    resolve({ error: 'Terser worker exited unexpectedly' });
+                };
+                const waitResult = msg => {
+                    if (msg.type === 'result') {
+                        cleanup();
+                        resolve(msg.result);
+                    }
+                };
+                worker.on('message', waitResult);
+                worker.on('close', handleClose);
+                worker.send({ type: 'run', options: serialize_javascript_1.default(options) });
+            });
+            this.workers.push(worker);
+            return result;
+        }
+        else {
+            // Just run in current process
+            return minify_1.default(options);
+        }
+    }
+    run(tasks, callback) {
+        /* istanbul ignore if */
+        if (!tasks.length) {
+            callback(null, []);
+            return;
+        }
+        let toRun = tasks.length;
+        const results = [];
+        const step = (index, data) => {
+            toRun -= 1;
+            results[index] = data;
+            if (!toRun) {
+                callback(null, results);
+            }
+        };
+        tasks.forEach((task, index) => {
+            const enqueue = async () => {
+                await this.sema.acquire();
+                this.runTask(task).then(result => {
+                    const done = () => {
+                        this.sema.release();
+                        step(index, result);
+                    };
+                    if (this.cacheDir && !result.error) {
+                        cacache_1.default
+                            .put(this.cacheDir, serialize_javascript_1.default(task.cacheKeys), JSON.stringify(result))
+                            .then(done, done);
+                    }
+                    else {
+                        done();
+                    }
+                });
+            };
+            if (this.cacheDir) {
+                cacache_1.default
+                    .get(this.cacheDir, serialize_javascript_1.default(task.cacheKeys))
+                    .then(({ data }) => step(index, JSON.parse(data)), enqueue);
+            }
+            else {
+                enqueue();
+            }
+        });
+    }
+    exit() {
+        this.workers.forEach(worker => worker.kill());
+        this.sema.drain();
+    }
+}
+exports.default = TaskRunner;
---
+++ dist/build/webpack/plugins/terser-webpack-plugin/src/worker.js
@@ -0,0 +1,23 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const minify_1 = __importDefault(require("./minify"));
+process.addListener('message', msg => {
+    if (msg.type === 'run') {
+        try {
+            // 'use strict' => this === undefined (Clean Scope)
+            // Safer for possible security issues, albeit not critical at all here
+            // eslint-disable-next-line no-new-func, no-param-reassign
+            const options = new Function('exports', 'require', 'module', '__filename', '__dirname', `'use strict'\nreturn ${msg.options}`)(exports, require, module, __filename, __dirname);
+            process.send({ type: 'result', result: minify_1.default(options) });
+        }
+        catch (error) {
+            process.send({ type: 'result', result: { error } });
+        }
+    }
+});
+process.send({ type: 'ready' });
+// keep process alive
+setInterval(() => { }, 30 * 1000);
